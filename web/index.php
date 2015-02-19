a26128b5c53c972e1dfcf4f0b03f533ef5fbfc19
Adam Bowles <bowlesa@aston.ac.uk>
2015-02-19 22:30:56 +0000

    remove test

--- web/index.php
+++ web/index.php
@@ -3,12 +3,11 @@
   // Configuration file
   include('../source/configuration/config.php');
 
+  // Abstract page template
   include('../source/classes/page.php');
 
   $page = new page();
 
   echo($page->construct_page());
-
-echo ROOT_DIRECTORY; // test
 
 ?>
