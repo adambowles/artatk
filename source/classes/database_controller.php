04b0424aa70da99bed4f10ff971f73eb5a25df31
Adam Bowles <bowlesa@aston.ac.uk>
2015-02-21 23:30:55 +0000

    db connection test code

--- source/classes/database_controller.php
+++ source/classes/database_controller.php
@@ -6,6 +6,9 @@
     public function __construct()
     {
       mysqli_connect(database_url,database_read_user,database_read_password,database_dbname);
+      if (mysqli_connect_errno() {
+        echo("Failed to connect to MySQL: " . mysqli_connect_error());
+      }
     }
   }
 
