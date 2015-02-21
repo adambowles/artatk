7216f57c86aeae3169a4c027dd7099c688c64c0e
Adam Bowles <bowlesa@aston.ac.uk>
2015-02-21 23:31:54 +0000

    missing bracket

--- source/classes/database_controller.php
+++ source/classes/database_controller.php
@@ -6,7 +6,7 @@
     public function __construct()
     {
       mysqli_connect(database_url,database_read_user,database_read_password,database_dbname);
-      if (mysqli_connect_errno() {
+      if (mysqli_connect_errno()) {
         echo("Failed to connect to MySQL: " . mysqli_connect_error());
       }
     }
