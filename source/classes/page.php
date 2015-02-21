79f8639994f9f46cc00a79fae0d040231bf397b9
Adam Bowles <bowlesa@aston.ac.uk>
2015-02-21 18:15:53 +0000

    restructure content modifying

--- source/classes/page.php
+++ source/classes/page.php
@@ -1,15 +1,21 @@
 <?php
 
   /**
-   * 
+   *
    */
   class page
   {
 
     private $asset_controller;
 
+    // Strings of page content, append whatever you like in order to add it to the page
+    // (hint: user the add_header()/add_body()/add_footer() funcs)
+    private $head;
+    private $body;
+    private $footer;
+
     /**
-     * 
+     *
      */
     public function __construct()
     {
@@ -23,10 +29,12 @@
         $rd .= "../";
         $i++;
 
-        // Just in case README.md and LICENSE were deleted for whatever reason, add an infinite loop breakout
+        // Just in case README.md and LICENSE were deleted for
+        // whatever reason, add an infinite loop breakout
         if ($i>255) {
           $rd = "";
-          die("Files \"README.md\" and \"LICENSE\" weren't found, did you delete them?<br>Put them back, they're necessary!");
+          die("Files \"README.md\" and \"LICENSE\" weren't found," .
+              " did you delete them?<br>Put them back, they're necessary!");
         }
       };
 
@@ -38,17 +46,19 @@
     }
 
     /**
-     * 
+     *
      */
-    public function construct_page()
+    public function get_html()
     {
-      return $this->construct_header() . $this->construct_body() . $this->construct_footer();
+      return $this->construct_head() .
+        $this->construct_body() .
+        $this->construct_footer();
     }
 
     /**
-     * 
+     *
      */
-    private function construct_header()
+    private function construct_head()
     {
       return "<!doctype html>\r\n" .
              "<html>\r\n" .
@@ -58,21 +68,38 @@
              "</title>\r\n" .
              $this->asset_controller->get_bootstrap_css() .
              $this->asset_controller->get_css() .
+             $this->head .
              "</head>\r\n" .
              "<body>\r\n"
         ;
     }
 
     /**
-     * 
+     *
      */
-    private function construct_body()
+    private function add_head($content)
     {
-      return "";
+      $this->head = $this->head . $content;
     }
 
     /**
-     * 
+     *
+     */
+    private function construct_body()
+    {
+      return $this->body;
+    }
+
+    /**
+     *
+     */
+    private function add_body($content)
+    {
+      $this->body = $this->body . $content;
+    }
+
+    /**
+     *
      */
     private function construct_footer()
     {
@@ -80,15 +107,24 @@
              "<footer>\r\n" .
              $this->asset_controller->get_js() .
              $this->asset_controller->get_jQuery() .
+             $this->footer .
              "</footer>\r\n" .
              "</html>\r\n"
         ;
     }
 
+    /**
+     *
+     */
+    private function add_footer($content)
+    {
+      $this->footer = $this->footer . $content;
+    }
+
   }
 
   /**
-   * 
+   *
    */
   class rating_page extends page
   {
