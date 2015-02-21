1d2ac565e4d6438de6df02b342e4135aedc22519
Adam Bowles <bowlesa@aston.ac.uk>
2015-02-21 19:56:53 +0000

    add HTTP 404 code

--- web/404.php
+++ web/404.php
@@ -1,16 +1,14 @@
 <?php
-  // Future considerations for this file:
-  // 1. Turn it into a front controller to handle ALL site requests
-  // and handle passing those requests to the right files in the source folder
-  // 2.
 
   // Configuration file
   include('../source/configuration/config.php');
 
   // Abstract page template
-  include('../source/classes/page.php');
+  include(ROOT_DIRECTORY . 'source/classes/page.php');
 
   $page = new error_404_page();
   $page->print_html();
 
+  // Return proper error code
+  header("HTTP/1.0 404 Not Found");
 ?>
