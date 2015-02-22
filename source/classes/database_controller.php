49d7e964c23f631921869f79760477482f1a7198
Adam Bowles <bowlesa@aston.ac.uk>
2015-02-22 10:56:08 +0000

    refactor constructor and destructor
    private field stores the connection and methods handle connecting to
    read / write accounts
    add method to check if connection is active
    add disconnector that will close the connection only if isn’t already
    closed

--- source/classes/database_controller.php
+++ source/classes/database_controller.php
@@ -7,19 +7,78 @@
    * Start each method off by sanitising it's parameters. Help avoid SQL injections
    *
    * Avoid using exposed SQL anywhere in this class. Write stored procedures instead.
+   *
+   * @author Adam Bowles <bowlesa@aston.ac.uk>
    */
   class database_controller
   {
+
+    private $connection;
 
     /**
      *
      */
     public function __construct()
     {
-      mysqli_connect(database_url,database_read_user,database_read_password,database_dbname);
-      if (mysqli_connect_errno()) {
-        echo("Failed to connect to MySQL: " . mysqli_connect_error());
+
+    }
+
+    /**
+     *
+     */
+    public function connect_read()
+    {
+      if($this->is_connected()){
+        mysqli_close($this->connection);
       }
+
+      $this->connection = mysqli_connect(database_url, database_write_user, database_write_password, database_dbname);
+
+      if (mysqli_connect_errno()) {
+        die("Failed to connect to Database: " . mysqli_connect_error());
+      }
+    }
+
+    /**
+     *
+     */
+    public function connect_write()
+    {
+      if($this->is_connected()){
+        mysqli_close($this->connection);
+      }
+
+      $this->connection = mysqli_connect(database_url, database_write_user, database_write_password, database_dbname);
+
+      if (mysqli_connect_errno()) {
+        die("Failed to connect to Database: " . mysqli_connect_error());
+      }
+    }
+
+    /**
+     *
+     */
+    public function is_connected()
+    {
+      return !$this->connection == null;
+    }
+
+    /**
+     * Destroy the connection and set it to null
+     */
+    public function disconnect()
+    {
+      if($this->is_connected()) {
+        $this->connection->close();
+        $this->connection = null;
+      }
+    }
+
+    /**
+     *
+     */
+    function __destruct() {
+      $this->disconnect();
     }
 
   }
