df7044a450be46bfb33e076a35e1148097c6b747
Adam Bowles <bowlesa@aston.ac.uk>
2015-02-22 00:25:40 +0000

    add tips comments

--- source/classes/database_controller.php
+++ source/classes/database_controller.php
@@ -1,8 +1,19 @@
 <?php
 
+  /**
+   * Tips for writing functions in this class.
+   * Security and attention to what you're writing here is very important
+   *
+   * Start each method off by sanitising it's parameters. Help avoid SQL injections
+   *
+   * Avoid using exposed SQL anywhere in this class. Write stored procedures instead.
+   */
   class database_controller
   {
 
+    /**
+     *
+     */
     public function __construct()
     {
       mysqli_connect(database_url,database_read_user,database_read_password,database_dbname);
@@ -10,6 +21,7 @@
         echo("Failed to connect to MySQL: " . mysqli_connect_error());
       }
     }
+
   }
 
 ?>
