d2f187e31f39c798a3d4c631b766fde1e19a09f9
Adam Bowles <bowlesa@aston.ac.uk>
2015-02-23 16:55:29 +0000

    password complexity checker

--- web/assets/js/register/register.js
+++ web/assets/js/register/register.js
@@ -13,19 +13,35 @@ function validate_form(form)
 
 function validate_input(value, as)
 {
+  // email address
   if(as == 'email') {
     email_regex = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i;
     return email_regex.test(value);
   }
 
+  // Password
   if(as == 'password') {
-    return false; //TODO: pw complexity checker, then hash it and set a hidden field to tell the POST that the pw has been hashed already
+    var matching_rules = 0;
+
+    if(value.length < 8) {
+      return false; // Password less than 8 characters
+    } else {
+      macthing_rules++;
+    }
+
+    /[a-z]/.test(value) ? matching_rules++ : null; // Password contains
+    /[A-Z]/.test(value) ? matching_rules++ : null;
+    /\d/.test(value) ? matching_rules++ : null;
+    /\W/.test(value) ? matching_rules++ : null;
+
+    return matching_rules >= 3; //TODO: pw complexity checker, then hash it and set a hidden field to tell the POST that the pw has been hashed already
   }
 
+  // Numerical value
   if(as == 'number') {
     return !isNaN(value);
   }
 
-  // all other cases test value as string
+  // All other cases test value as string
   return "" + value.length > 0;
 }
